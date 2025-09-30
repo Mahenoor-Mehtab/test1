2be27ea7027b5de21ae8d74deaaf847e0c3b4227
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2025-09-30 19:00:59 +0530

    change in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Main from "./components/main";
 const App = () => {
   return (
     <>
+    <h1>Helllo</h1>
       <div>Code Rabbit Testing</div>
       <Main />
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis consequuntur rem vitae voluptatibus doloremque iste corrupti facere quis saepe unde. A sequi incidunt magnam ex enim facilis reiciendis ipsa adipisci!
+      </p>
     </>
   );
 };
