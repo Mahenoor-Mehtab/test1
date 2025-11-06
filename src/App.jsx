2071ead823dabb37696c5b911c7f45cd644cce5f
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2025-11-06 10:37:22 +0530

    theme changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
     <>
     <h1>Helllo</h1>
       <div>Code Rabbit Testing</div>
-      <Main />
+
       <h1>hello world</h1>
-     <Nav/>
+    
       <p></p>
     </>
   );
--- src/components/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = () => {
-  return (
-    <div>Here Is my Main Componente</div>
-  )
-}
-
-export default Main
